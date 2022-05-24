e057a3352ebd6d86ce48dafa093c359c4d32ccfd
Igor Lima <igorlimaesilva94@gmail.com>
2022-05-23 22:37:24 -0300

    Initial commit

---
+++ app/src/main/java/com/droidev/flyffuwebviewclient/MainActivity.java
@@ -0,0 +1,90 @@
+package com.droidev.flyffuwebviewclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    LinearLayout linearLayout;
+    Boolean exit = false;
+
+    String url = "https://universe.flyff.com/play";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
+
+        View decorView = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        linearLayout = findViewById(R.id.LinearLayout);
+
+        webView = new WebView(getApplicationContext());
+
+        webView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        linearLayout.addView(webView);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                if (url.equals(url)) {
+                    view.loadUrl(url);
+                }
+
+                return true;
+            }
+        });
+
+        WebSettings webSettingsStream = webView.getSettings();
+        webSettingsStream.setJavaScriptEnabled(true);
+        webSettingsStream.setDomStorageEnabled(true);
+        webSettingsStream.setJavaScriptCanOpenWindowsAutomatically(false);
+        webSettingsStream.setUseWideViewPort(true);
+        webSettingsStream.setLoadWithOverviewMode(true);
+
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish();
+        } else {
+            Toast.makeText(this, "Press Back again to Exit.",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(() -> exit = false, 3 * 1000);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        linearLayout.removeAllViews();
+        webView.destroy();
+    }
+}
