9e99a7f1e8977f137d6e7668773c4215e585f5db
Igor Lima <igorlimaesilva94@gmail.com>
2022-05-25 21:49:48 -0300

    Added dual client.

--- app/src/main/java/com/droidev/flyffuwebviewclient/MainActivity.java
+++ app/src/main/java/com/droidev/flyffuwebviewclient/MainActivity.java
@@ -5,24 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    WebView webView;
+    WebView mClientWebView, sClientWebView;
+    FrameLayout mClient, sClient;
     LinearLayout linearLayout;
     Boolean exit = false;
 
+    Menu optionMenu;
+
     String url = "https://universe.flyff.com/play";
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,24 +39,138 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        getSupportActionBar().hide();
+        fullScreenOn();
+
+        linearLayout = findViewById(R.id.linearLayout);
+
+        mClient = findViewById(R.id.frameLayoutMainClient);
+
+        sClient = findViewById(R.id.frameLayoutSecondClient);
+
+        mClientWebView = new WebView(getApplicationContext());
+
+        sClientWebView = new WebView(getApplicationContext());
+
+        mainClient();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish();
+        } else {
+            Toast.makeText(this, "Press Back again to Exit.",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            fullScreenOff();
+            new Handler().postDelayed(() -> exit = false, 3 * 1000);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mClient.removeAllViews();
+        sClient.removeAllViews();
+        mClientWebView.destroy();
+        sClientWebView.destroy();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.secondClient:
+
+                if (sClient.getVisibility() == View.GONE) {
+
+                    sClient.setVisibility(View.VISIBLE);
+
+                    secondClient();
+
+                    optionMenu.findItem(R.id.secondClient).setTitle("Close Second Client");
+                } else {
+
+                    sClient.removeAllViews();
+
+                    sClientWebView.loadUrl("about:blank");
+
+                    sClient.setVisibility(View.GONE);
+
+                    optionMenu.findItem(R.id.secondClient).setTitle("Open Second Client");
+                }
+
+                break;
+
+            case R.id.fullScreen:
+
+                fullScreenOn();
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+
+        optionMenu = menu;
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void fullScreenOn() {
 
         View decorView = getWindow().getDecorView();
 
+        getSupportActionBar().hide();
+
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
+    }
 
-        linearLayout = findViewById(R.id.LinearLayout);
+    private void fullScreenOff() {
 
-        webView = new WebView(getApplicationContext());
+        View decorView = getWindow().getDecorView();
+
+        getSupportActionBar().show();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    private void mainClient() {
+
+        createWebViewer(mClientWebView, mClient);
+    }
+
+    private void secondClient() {
+
+        createWebViewer(sClientWebView, sClient);
+    }
+
+    @SuppressLint({"SetJavaScriptEnabled", "ClickableViewAccessibility"})
+    private void createWebViewer(WebView webView, FrameLayout frameLayout) {
 
         webView.setLayoutParams(new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
 
-        webView.requestFocusFromTouch();
+        webView.requestFocus(View.FOCUS_DOWN);
+        webView.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_UP:
+                    if (!v.hasFocus()) {
+                        v.requestFocus();
+                    }
+                    break;
+            }
+            return false;
+        });
 
-        linearLayout.addView(webView);
+        frameLayout.addView(webView);
 
         webView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -70,24 +192,5 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setUserAgentString("FlyffU WebViewClient");
 
         webView.loadUrl(url);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (exit) {
-            finish();
-        } else {
-            Toast.makeText(this, "Press Back again to Exit.",
-                    Toast.LENGTH_SHORT).show();
-            exit = true;
-            new Handler().postDelayed(() -> exit = false, 3 * 1000);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        linearLayout.removeAllViews();
-        webView.destroy();
     }
 }
