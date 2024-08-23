14fa9f6072136f59a0d9757a45f49197254ee092
None <None@none>
2024-08-22 22:16:05 -0300

    will now get system default agent.

--- app/src/main/java/com/droidev/flyffuwebviewclient/MainActivity.java
+++ app/src/main/java/com/droidev/flyffuwebviewclient/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     TinyDB tinyDB;
 
-    String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+    String userAgent = System.getProperty("http.agent");
 
     Menu optionMenu;
 
